f750082821c670000afd9a3499c412db044ca8a8
marcos <masadu@outlook.es>
2023-05-02 14:00:03 +0200

    botones

--- js/app.js
+++ js/app.js
@@ -1,1 +1,41 @@
 // Tu código aquí.
+
+let counter = 0;
+
+document.querySelector(".nextBtn").addEventListener("click",() => {
+
+    counter++;
+
+    document.querySelector("#counter").innerHTML= counter;
+    if(counter > 0 ){
+        document.querySelector("#counter").style.color= "green";
+
+    }
+        
+    else if(counter < 0){
+        document.querySelector("#counter").style.color= "red";
+    }
+
+    else{counter = 0 
+        document.querySelector("#counter").style.color= "grey";
+    }
+});
+
+document.querySelector(".prevBtn").addEventListener("click",() => {
+
+    counter--;
+
+    document.querySelector("#counter").innerHTML= counter;
+    if(counter > 0 ){
+        document.querySelector("#counter").style.color= "green";
+
+    }
+        
+    else if(counter < 0){
+        document.querySelector("#counter").style.color= "red";
+    }
+
+    else{counter = 0 
+        document.querySelector("#counter").style.color= "grey";
+    }
+})
